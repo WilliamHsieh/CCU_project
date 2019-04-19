23e4a2a6e94cfb5f2eaa65f87e2345ae47c76a36
williamhsieh <wh31110@gmail.com>
2019-04-19 22:05:45 +0800

    modulize

--- training.py
+++ training.py
@@ -1,101 +1,113 @@
-# Import the libraries
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
+## Import the libraries
 import csv
-
-# Feature Scaling
-from sklearn.preprocessing import MinMaxScaler
-scaler_list = []
-scaler_list.append(MinMaxScaler(feature_range = (0, 1))) #close
-scaler_list.append(MinMaxScaler(feature_range = (0, 1))) #volumn
-
-# Import the training set
-dataset_train = pd.read_csv('stock_data_train.csv')
-training_set = []
-training_set.append(dataset_train.iloc[:, 4:5].values)  # 'close'
-training_set.append(dataset_train.iloc[:, 5:6].values)  # 'close'
-
-# scale training set
-training_set_scaled = []
-for x in range(len(training_set)):
-    training_set_scaled.append(scaler_list[x].fit_transform(training_set[x]))
-
-# combine all dim. input
-training_data = []
-for data_length in range(len(training_set_scaled[0])):
-    tmp = []
-    for data_count in range(len(training_set_scaled)):
-        tmp.append(np.ndarray.tolist(training_set_scaled[data_count][data_length])[0])
-    training_data.append(tmp)
-training_data = np.array(training_data)
-
-x_train = []   # 60 days data
-y_train = []   # predict 61th
-for i in range(60, 1199):  # 1199 is total data amount
-    x_train.append(training_data[i-60:i])
-    y_train.append(training_data[i])
-
-x_train, y_train = np.array(x_train), np.array(y_train)
-# x_train = np.reshape(x_train, (x_train.shape[0], x_train.shape[1], x_train.shape[2]))
-# y_train = np.reshape(y_train, (y_train.shape[0], y_train.shape[1], 1))
-# print(f'timesteps: {x_train.shape[1]}')
-
-# Import the Keras libraries and packages
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers import Dropout
-
-# Initialising the RNN
-model = Sequential()
-
-# Adding the first LSTM layer and some Dropout regularisation
-model.add(LSTM(units = 50, return_sequences = True, input_shape = (x_train.shape[1], x_train.shape[2])))
-model.add(Dropout(0.2))
-
-# Adding a second LSTM layer and some Dropout regularisation
-model.add(LSTM(units = 50, return_sequences = True))
-model.add(Dropout(0.2))
-
-# Adding a fourth LSTM layer and some Dropout regularisation
-model.add(LSTM(units = 50))
-model.add(Dropout(0.2))
-
-# Adding the output layer
-model.add(Dense(units = 2))
-
-# Visualize the model
-from keras.utils import plot_model
-plot_model(model, to_file='model.png', show_shapes=True, show_layer_names=True)
-
-# Compiling
-model.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics=['accuracy'])
-print(model.summary())
-
-# Function for storing losses
-from keras.callbacks import Callback
-class LossHistory(Callback):
-    def on_train_begin(self, logs={}):
-        self.losses = []
-
-    def on_batch_end(self, batch, logs={}):
-        self.losses.append(logs.get('loss'))
-
-# Start training && save model/losses
-# history = LossHistory()
-# history = model.fit(x_train, y_train, epochs = 10, batch_size = 32, callbacks=[history])
-history = model.fit(x_train, y_train, epochs = 100, batch_size = 32, validation_split=0.2, shuffle=True)
-# list all data in history
-# print(history.history.keys())
-
 import json
-with open('loss_acc.json', 'w') as outfile:  
-    json.dump(history.history, outfile)
-# plot history for loss
-plt.plot(history.history['loss'])
-plt.title('model loss')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.show()
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from keras.layers import LSTM
+from keras.layers import Dense
+from keras.layers import Dropout
+from keras.models import Sequential
+from sklearn.preprocessing import MinMaxScaler
+
+## Function
+### Variable
+scaler_list = []
+training_set = []
+training_set_scaled = []
+x_train = []
+y_train = []
+total_epochs = 50
+batchSize = 32
+
+### Feature Scaling
+def feature_scaling():
+    # 0. close
+    # 1. volumn
+    global scaler_list
+    scaler_list.append(MinMaxScaler(feature_range = (0, 1)))
+    scaler_list.append(MinMaxScaler(feature_range = (0, 1)))
+
+### Import the training set
+def get_training_data():
+    global training_set
+    dataset_train = pd.read_csv('./data/stock_data_train.csv')
+    training_set.append(dataset_train.iloc[:, 4:5].values)  # close
+    training_set.append(dataset_train.iloc[:, 5:6].values)  # volumn
+
+### Scale training set
+def scale_data():
+    global training_set_scaled
+    for x in range(len(training_set)):
+        training_set_scaled.append(scaler_list[x].fit_transform(training_set[x]))
+
+### Orginize data (x_train, y_train)
+def orginize_data():
+    # combine all dimension data
+    training_data = []
+    for data_length in range(len(training_set_scaled[0])):
+        tmp = []
+        for data_count in range(len(training_set_scaled)):
+            tmp.append(np.ndarray.tolist(training_set_scaled[data_count][data_length])[0])
+        training_data.append(tmp)
+    training_data = np.array(training_data)
+
+    # get x_train, y_train
+    global x_train, y_train
+    for i in range(60, 1199):  # 1199 is total data amount
+        x_train.append(training_data[i-60:i])
+        y_train.append(training_data[i])
+
+    x_train, y_train = np.array(x_train), np.array(y_train)
+
+### model structure
+def model_structure():
+    # Initialising the RNN
+    model = Sequential()
+
+    # Adding the first LSTM layer and some Dropout regularisation
+    model.add(LSTM(units = 50, return_sequences = True, input_shape = (x_train.shape[1], x_train.shape[2])))
+    model.add(Dropout(0.2))
+
+    # Adding a second LSTM layer and some Dropout regularisation
+    model.add(LSTM(units = 50, return_sequences = True))
+    model.add(Dropout(0.2))
+
+    # Adding a third LSTM layer and some Dropout regularisation
+    model.add(LSTM(units = 50))
+    model.add(Dropout(0.2))
+
+    # Adding the output layer
+    model.add(Dense(units = 2))
+
+    # Compiling
+    model.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics=['accuracy'])
+
+    # Fit
+    history = model.fit(x_train, y_train, epochs = total_epochs, batch_size = batchSize, shuffle=True)
+
+    # save model
+    model.save('./data/my_model.h5')
+    
+    return history
+
+    # Visualize the model
+#     from keras.utils import plot_model
+#     plot_model(model, to_file='model.png', show_shapes=True, show_layer_names=True)
+#     print(model.summary())
+
+### saving result
+def result(history):
+    # save the result to json file
+    with open('./data/loss_and_acc.json', 'w') as outfile:
+        json.dump(history.history, outfile)
+
+## Main function
+if __name__ == "__main__":
+    feature_scaling()
+    get_training_data()
+    scale_data()
+    orginize_data()
+    history = model_structure()
+    result(history)
 
