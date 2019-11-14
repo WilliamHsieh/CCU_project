30c15cd310512b8a84706936ae50cacfd4a8bb70
williamhsieh <wh31110@gmail.com>
2019-11-14 23:09:21 +0800

    adding predict and draw function

--- cal.py
+++ cal.py
@@ -1,9 +1,14 @@
+## import
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import preprocess as P
+
+from sklearn.metrics import r2_score
+from scipy.stats import pearsonr
 
 ## calculate correlation
 def correlation():
-    from scipy.stats import pearsonr
     data_type = "train"
 
     raw_data = []
@@ -21,53 +26,57 @@ def correlation():
     raw_data.append(csv_data.iloc[:, 4:5].values)  # close
 
 # calculate Pearson's correlation
-    raw_data = list(np.array(raw_data).reshape(4, -1))
+    raw_data = np.array(raw_data).reshape(4, -1)
     corr, _ = pearsonr(raw_data[0], raw_data[2])
     print('stock(close) / nasdaq : %.3f' % corr)
     corr, _ = pearsonr(raw_data[0], raw_data[3])
     print('stock(close) / dji : %.3f' % corr)
+    corr, _ = pearsonr(raw_data[2], raw_data[3])
+    print('nasdaq / dji : %.3f' % corr)
 
 ## calculate r2
 def r2():
-    import os
-    from keras import backend as K
-    from keras.models import load_model
-    from sklearn.metrics import r2_score
-    from preprocess import getGT, getData
-    pass
     epoch = 300
     dim = 4
-    win = 49
+    win = 60
     pred = 20
     freq = 5
+    fname = ""
+#     model_name = f"./model/draw/123/{epoch}_{dim}_{win}_{freq}.h5"
+
     # Get data
-    real_stock_price = getGT(pred, freq)
-    [x_test, y_test], scaler_list = getData(dim, win, pred, freq, "test")
-
-    # Model predict
-    K.clear_session()
-    model_name = f"./model/draw/epoch_{epoch},dim_{dim},win_{win},freq_{freq}.h5"
-    if not os.path.isfile(model_name):
-        return "no such model!"
-
-    model = load_model(model_name)
-    model_output = model.predict(x_test)
-
-    # get all the close price
-    close_price = []
-    for j in range(len(model_output)):
-        close_price.append(model_output[j][0])
-
-    # re-scale back
-    close_price = np.reshape(close_price, (1, -1))
-    predicted_stock_price = scaler_list[0].inverse_transform(close_price)[0]
+    real_price, pred_price = P.getGTandPred(epoch, dim, win, pred, freq, fname)
+    print(real_price)
+    print(pred_price)
     
     # r square
-    print(r2_score(real_stock_price, predicted_stock_price))
-    print(r2_score(real_stock_price, real_stock_price))
+    print(r2_score(real_price, pred_price))
+    P.drawTrend(real_price, pred_price, pred)
 
+## draw boxplot
+def drawBoxplot():
+    draw = []
+
+    real_price, pred_price = P.getGTandPred(300, 4, 60, 21, 1, ",default")
+    draw += [[real_price[i] - real_price[i+1] for i in range(20)]]      #baseline
+    draw += [[pred_price[i+1] - real_price[i+1] for i in range(20)]]    #predict
+
+    real_price, pred_price = P.getGTandPred(300, 4, 60, 21, 1, ",3y")
+    draw += [[pred_price[i+1] - real_price[i+1] for i in range(20)]]
+
+    real_price, pred_price = P.getGTandPred(300, 4, 60, 21, 1, ",1y")
+    draw += [[pred_price[i+1] - real_price[i+1] for i in range(20)]]
+
+#     axes.set_ylabel('err')
+    fig1, axes = plt.subplots()
+    axes.set_title('Box Plot')
+    axes.boxplot(draw)
+    plt.xticks(range(1, len(draw)+1), ["baseline", "6y", "3y", "1y"])
+    plt.show()
+    
 ## main
 if __name__ == "__main__":
-    correlation()
-    r2()
+#     correlation()
+#     r2()
+    drawBoxplot()
 
--- preprocess.py
+++ preprocess.py
@@ -43,7 +43,7 @@ def scaleData(input_dim, scaler_list, raw_data):
 
 ### Orginize data
 def orginizeData(input_dim, window_size, predict_days, data_frequency,
-        data_type, scaled_data, classify_flag):
+        data_type, scaled_data, classify_flag, data_size):
 
     # combine all dimension data
     total_data = []
@@ -56,11 +56,12 @@ def orginizeData(input_dim, window_size, predict_days, data_frequency,
     # deal with test input
     if data_type == "test":
         total_data = total_data[len(total_data) - predict_days - window_size:]
+    elif data_size + window_size <= len(total_data):
+        total_data = total_data[len(total_data) - data_size - window_size:]
 
     # get x_data, y_data
     x_data = []
     y_data = []
-    #TODO: set the input dataset to fix size
     for i in range(window_size, len(total_data)):
         x_data.append(total_data[i-window_size:i])
         if classify_flag:
@@ -77,7 +78,7 @@ def orginizeData(input_dim, window_size, predict_days, data_frequency,
 ## API
 ### Get data
 def getData(input_dim=4, window_size=60, predict_days=20, data_frequency=1, 
-        data_type="train", classify_flag=False):
+        data_type="train", classify_flag=False, data_size=1500):
 
     # 1. feature scaling
     scaler_list = featureScaling(input_dim)
@@ -90,7 +91,7 @@ def getData(input_dim=4, window_size=60, predict_days=20, data_frequency=1,
 
     # 4. return data
     return orginizeData(input_dim, window_size, predict_days, data_frequency, 
-            data_type, scaled_data, classify_flag), scaler_list
+            data_type, scaled_data, classify_flag, data_size), scaler_list
 
 ### Get ground truth
 def getGT(predict_days=20, data_frequency=1):
@@ -117,18 +118,78 @@ def getBase(predict_days = 20, data_frequency = 1):
 
     return MSE
 
+### Get predicted price
+def getPrice(epoch=300, dim=4, win=60, pred=20, freq=1, fname=""):
+    import os
+    from keras import backend as K
+    from keras.models import load_model
+    
+    # Get data
+    [x_test, y_test], scaler_list = getData(dim, win, pred, freq, "test")
+
+    # Model predict
+    K.clear_session()
+    model_name = f"./model/epoch_{epoch},dim_{dim},win_{win},freq_{freq}" + fname + f"/epoch_{epoch-1}.h5"
+    if not os.path.isfile(model_name):
+        return "no such model!"
+
+    model = load_model(model_name)
+    model_output = model.predict(x_test)
+
+    # get all the close price
+    close_price = []
+    for j in range(len(model_output)):
+        close_price.append(model_output[j][0])
+
+    # re-scale back
+    close_price = np.reshape(close_price, (1, -1))
+    predicted_stock_price = scaler_list[0].inverse_transform(close_price)[0]
+    return predicted_stock_price
+
+### Get GT && Pred
+def getGTandPred(epoch=300, dim=4, win=60, pred=20, freq=1, fname=""):
+    real_price = getGT(pred, freq)
+    pred_price = getPrice(epoch, dim, win, pred, freq, fname)
+    return real_price, pred_price
+
+## Draw trend
+def drawTrend(real_price, pred_price, pred_days):
+    import matplotlib.pyplot as plt
+    label1 = "Real Stock Price"
+    label2 = "Predicted Stock Price"
+    #marker = "p"
+
+    plt.clf()
+    plt.style.use("ggplot")   # beautiful shit
+    plt.title('predicted stock price')
+    plt.plot(real_price, 'bo-', label = label1)
+    plt.plot(pred_price, 'ro-', label = label2, marker = "^")
+    plt.xlabel('Time')
+    plt.ylabel('Stock Price')
+    plt.xticks(range(pred_days), [i+1 for i in range(pred_days)])
+    plt.legend()
+#     plt.savefig(filename + '.png')
+    plt.show()
+
 ## Main function
 if __name__ == "__main__":
     print("# get ground truth")
-    print(getGT())
+    real_price = getGT()
+    print(real_price)
 
     print("\n# get baseline value")
     print(getBase())
 
+    print("\n# get predicted price")
+    pred_price = getPrice(epoch=300, dim=4, win=49, pred=20, freq=5)
+    print(pred_price)
+
+#     input_dim, window_size, predict_days, data_frequency, data_type, classify_flag, data_size
     print("\n# get data")
-    # (input_dim, window_size, predict_days, data_frequency, data_type, classify_flag)
-#     [x, y], s = getData(4, 60, 20, 1, "train", False) #default
-    [x, y], s = getData(4, 49, 20, 5, "test", False)
+    [x, y], s = getData(4, 60, 20, 1, "train", False, 1500) #default
     print(x.shape)
     print(y.shape)
+
+    print("\n# draw trend")
+    print(drawTrend(real_price, pred_price, pred_days=20))
 
